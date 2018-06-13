c28173ec0f874320baf4ac31220e390aff869128
Ghost <kubafilinger@gmail.com>
2018-06-13 16:03:41 +0200

    Refactor usart lib

--- usart/main/main.c
+++ usart/main/main.c
@@ -1,10 +1,10 @@
 #define F_CPU 8000000L
 
-#include "usart/usart.c"
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
+#include "usart.h"
 
 int main(void)
 {
@@ -16,13 +16,14 @@ int main(void)
 	float f = 1202.9377;
 	uint32_t i = 12345678;
 	char* s = "This is char";
-	
-	sprintf(txt, "float = %f, unsigned long = %lu, text = %s\r\n", f, i, s);
+
+	//sprintf(txt, "float = %f, unsigned long = %lu, text = %s\r\n", f, i, s);
+	sprintf(txt, "test usart");
 
 	while(1)
 	{
 		send(txt);
-		
+
 		_delay_ms(1000);
 	}
 }
