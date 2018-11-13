d2b0ef03f132192327a0485b3d64bb99e7c010b0
Ghost <kubafilinger@gmail.com>
2018-11-13 07:56:03 +0100

    Add Cpp lib

---
+++ led_multiplekser/led_multiplekser/led_driver_cpp/led_driver.h
@@ -0,0 +1,43 @@
+#ifndef __LED_DRIVER_H__
+#define __LED_DRIVER_H__
+
+#define MAX_NUM_DISPLAY 8
+#define ANODA 0
+#define KATODA 1
+#define PUT_DOT 1
+#define DEL_DOT 0
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+uint8_t total_displays;
+uint8_t position;
+uint8_t led_type;
+volatile uint8_t number[MAX_NUM_DISPLAY];
+volatile uint8_t *digit_ddr;
+volatile uint8_t *digit_port;
+volatile uint8_t *display_ddr;
+volatile uint8_t *display_port;
+
+void LEDInit(uint8_t total_displays_f, uint8_t led_type_f, volatile uint8_t *digit_ddr_wsk, volatile uint8_t *digit_port_wsk, volatile uint8_t *display_ddr_wsk, volatile uint8_t *display_port_wsk);
+
+void LEDSetValue(char *val);
+
+void LEDDot(uint8_t dot);
+
+void LEDSetNumber(int nr);
+
+// 1 - 9 -> 01 - 09
+void LEDSetNumberWithZero(int nr);
+
+// set position first char on screen. 0 - first, 1 - second, etc.
+void LEDSetPosition(uint8_t pos);
+
+void clearDisplay();
+
+uint8_t convertChar(char s);
+
+void setTimer0();
+
+#endif // __LED_DRIVER_H__
--- led_multiplekser/led_multiplekser/led_multiplekser.c
+++ led_multiplekser/led_multiplekser/led_multiplekser.c
@@ -8,42 +8,42 @@
 int main(void)
 {
 	LEDInit(6, 1, &DDRD, &PORTD, &DDRC, &PORTC);
-	
+
 	sei();
-	
+
 	LEDSetNumber(32);
 	clearDisplay();
 	LEDSetValue("7 312");
 	LEDSetPosition(2);
-	
+
 	LEDDot(PUT_DOT);
 	_delay_ms(2000);
-	
+
 	LEDDot(DEL_DOT);
 	_delay_ms(2000);
-	
+
 	char chars[30] = "ABbCcEFGHhIiJjLOoPSTtUuYZ=-_.";
-    
+
 	while(1)
     {
 		int static a = 0;
-			
-		clearDisplay();	
+
+		clearDisplay();
 		LEDSetValue("ABbCcd");
 		_delay_ms(5000);
-		
+
 		clearDisplay();
 		LEDSetValue("EFGHhI");
 		_delay_ms(5000);
-		
+
 		clearDisplay();
 		LEDSetValue("iJjLOo");
 		_delay_ms(5000);
-		
+
 		clearDisplay();
 		LEDSetValue("PSTtUu");
 		_delay_ms(5000);
-		
+
 		clearDisplay();
 		LEDSetValue("YZ_-.=");
 		_delay_ms(5000);
